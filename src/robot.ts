219f3a3a803fe7bf91f9889cd09cd901f8669d22
PanderaStudios <panderastudios@hotmail.com>
2025-10-04 01:57:37 -0300

    - mcheck

--- src/robot.ts
+++ src/robot.ts
@@ -18,18 +18,18 @@ export class RobotSystem extends createSystem({
   init() {
     this.lookAtTarget = new Vector3();
     this.vec3 = new Vector3();
-    // this.queries.robotClicked.subscribe("qualify", (entity) => {
-    //   AudioUtils.play(entity);
-    // });
+    this.queries.robotClicked.subscribe("qualify", (entity) => {
+      AudioUtils.play(entity);
+    });
   }
 
-  // update() {
-  //   this.queries.robot.entities.forEach((entity) => {
-  //     this.player.head.getWorldPosition(this.lookAtTarget);
-  //     const spinnerObject = entity.object3D!;
-  //     spinnerObject.getWorldPosition(this.vec3);
-  //     this.lookAtTarget.y = this.vec3.y;
-  //     spinnerObject.lookAt(this.lookAtTarget);
-  //   });
-  //}
+  update() {
+    this.queries.robot.entities.forEach((entity) => {
+      this.player.head.getWorldPosition(this.lookAtTarget);
+      const spinnerObject = entity.object3D!;
+      spinnerObject.getWorldPosition(this.vec3);
+      this.lookAtTarget.y = this.vec3.y;
+      spinnerObject.lookAt(this.lookAtTarget);
+    });
+  }
 }
